e5f1b7b437937428888ed6869f91c9af1fade1fa
X0lotl <danil200319771@gmail.com>
2022-06-25 15:44:40 +0300

    first Paint

---
+++ KhomichenkoPaint/src/DrawArea.java
@@ -0,0 +1,88 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class DrawArea extends JComponent {
+    private Image image;
+
+    private Graphics2D g2;
+
+    private int currentX, currentY, oldX, oldY;
+
+    public DrawArea() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                oldX = e.getX();
+                oldY = e.getY();
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                currentX = e.getX();
+                currentY = e.getY();
+
+                if (g2 != null) {
+                    g2.drawLine(oldX, oldY, currentX, currentY);
+                    repaint();
+                    oldX = currentX;
+                    oldY = currentY;
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if(image == null ) {
+            image = createImage(getSize().width, getSize().height);
+            g2 = (Graphics2D) image.getGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON );
+            clear();
+        }
+
+        g.drawImage(image, 0, 0, null);
+    }
+
+    public void clear() {
+        g2.setPaint(Color.white);
+        g2.fillRect(0, 0, getSize().width, getSize().height);
+        g2.setPaint(Color.black);
+        repaint();
+    }
+
+    public void setPaintColor(Color color) {
+        g2.setPaint(color);
+    }
+
+    public void black() {
+        g2.setPaint(Color.black);
+    }
+
+    public void red() {
+        g2.setPaint(Color.red);
+    }
+
+    public void magenta() {
+        g2.setPaint(Color.magenta);
+    }
+
+    public void green() {
+        g2.setPaint(Color.green);
+    }
+
+    public void blue() {
+        g2.setPaint(Color.blue);
+    }
+}
--- KhomichenkoPaint/src/Paint.java
+++ KhomichenkoPaint/src/Paint.java
@@ -1,13 +1,77 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class Paint {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new CGTemplate();
+public class Paint{
+    //HashMap<JButton, Color> colorButtons = new HashMap<>();
+    JButton clearBtn, blackBtn, blueBtn, greenBtn, redBtn, magentaBtn;
+    DrawArea drawArea;
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == clearBtn) {
+                drawArea.clear();
+            } else if (e.getSource() == blackBtn) {
+                drawArea.black();
+            } else if (e.getSource() == blueBtn) {
+                drawArea.blue();
+            } else if (e.getSource() == redBtn) {
+                drawArea.red();
+            } else if (e.getSource() == magentaBtn) {
+                drawArea.magenta();
+            } else if (e.getSource() == greenBtn) {
+                drawArea.green();
             }
-        });
+        }
+    };
+
+    public static void main(String[] args) {
+        new Paint().show();
+    }
+
+    public void show() {
+        JFrame frame = new JFrame("MyPaint");
+        Container content = frame.getContentPane();
+        content.setLayout(new BorderLayout());
+
+        drawArea = new DrawArea();
+
+        content.add(drawArea, BorderLayout.CENTER);
+
+        JPanel controls = new JPanel();
+
+        clearBtn  = new JButton("Clear");
+        clearBtn.addActionListener(actionListener);
+
+        blackBtn = new JButton("Black");
+        blackBtn.addActionListener(actionListener);
+
+        blueBtn = new JButton("Blue");
+        blueBtn.addActionListener(actionListener);
+
+        greenBtn = new JButton("Green");
+        greenBtn.addActionListener(actionListener);
+
+        redBtn = new JButton("Red");
+        redBtn.addActionListener(actionListener);
+
+        magentaBtn = new JButton("Magenta");
+        magentaBtn.addActionListener(actionListener);
+
+        controls.add(greenBtn);
+        controls.add(blackBtn);
+        controls.add(blueBtn);
+        controls.add(redBtn);
+        controls.add(magentaBtn);
+        controls.add(clearBtn);
+
+        content.add(controls, BorderLayout.SOUTH);
+
+        frame.setSize(600, 600);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
